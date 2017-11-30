bd5b3b4de0d307ed2de44397763fc2dafcd274db
Chappie2 <32395880+Chappie2@users.noreply.github.com>
2017-12-01 00:39:28 +0100

    Got some Google APIs running, and customized a map

--- Awesome.js
+++ Awesome.js
@@ -1,48 +1,20 @@
+var latitude = 51.9166667;
+var longitude = 4.5;
 $(() => {
     var ac = new google.maps.places.Autocomplete(document.getElementById('Banana'));
+
     console.log(ac);
     google.maps.event.addListener(ac, 'place_changed', function(){
         var place = ac.getPlace();
-        var latitude = place.geometry.location.lat;
-        var longitude = place.geometry.location.lng;
-        var googleUrl = place.url;
-        var searchAdress = place.formatted_address;
+        var latitude = place.geometry.location.lat();
+        var longitude = place.geometry.location.lng();
         console.log(place.formatted_address);
         console.log(place.url);
         console.log(place.geometry.location);
         console.log(latitude);
         console.log(longitude);
     });
-    //&callback=initMap
-    /*
-    $("#modal1").ready(function(){
-        // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-        $('.modal').modal();
-      });
-    // dropdown
-    $('.dropdown-button').dropdown({
-        inDuration: 300,
-        outDuration: 225,
-        constrainWidth: false, // Does not change width of dropdown to that of the activator
-        hover: true, // Activate on hover
-        gutter: 0, // Spacing from edge
-        belowOrigin: false, // Displays dropdown below the button
-        alignment: 'left', // Displays dropdown with edge aligned to the left of button
-        stopPropagation: false // Stops event propagation
-      }
-    );
-    // datepicker
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15, // Creates a dropdown of 15 years to control year,
-        today: 'Today',
-        clear: 'Clear',
-        close: 'Ok',
-        closeOnSelect: false // Close upon selecting a date,
-    }); 
-    $("#submit").on("click", () => {
-       alert()
-    })  */ 
+    
     $('#ExpandButton1').click(function(){
         $('#ExpandText1').toggle('slow');
     });  
