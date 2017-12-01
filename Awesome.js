8cdf8184b87cd0f58a16affed71e733c5de2bdb3
Chappie2 <32395880+Chappie2@users.noreply.github.com>
2017-12-01 13:09:16 +0100

    lod version I don't know how that happened

--- Awesome.js
+++ Awesome.js
@@ -1,18 +1,27 @@
-var latitude = 23.1135925;
-var longitude = -82.36659559999998;
 $(() => {
-    var ac = new google.maps.places.Autocomplete(document.getElementById('Banana'));
-    console.log(ac);
-    google.maps.event.addListener(ac, 'place_changed', function(){
-        var place = ac.getPlace();
-         latitude = place.geometry.location.lat();
-         longitude = place.geometry.location.lng();
-        console.log(place.formatted_address);
-        console.log(place.url);
-        console.log(place.geometry.location);
-        console.log(latitude);
-        console.log(longitude);
-    });
+    var latitude = 23.1135925;
+    var longitude = -82.36659559999998;
+    var coords = {lat: latitude, lng: longitude}
+    $(document).ready(function($){
+        var ac = new google.maps.places.Autocomplete(document.getElementById('Banana'));
+        console.log(ac);
+        google.maps.event.addListener(ac, 'place_changed', function(){
+            var place = ac.getPlace();
+             latitude = place.geometry.location.lat();
+             longitude = place.geometry.location.lng();
+            console.log(place.formatted_address);
+            console.log(place.url);
+            console.log(place.geometry.location);
+            console.log(latitude);
+            console.log(longitude);
+
+           $('#buttonSearch').on('click', function(e){
+           console.log('I am clicked');
+           addCoord(coords);
+         })})
+    
+   
+        });
             var styledMapType = new google.maps.StyledMapType(
                     [
                       {elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
@@ -146,28 +155,26 @@ $(() => {
                 map.mapTypes.set('styled_map', styledMapType);
                 map.setMapTypeId('styled_map');
               }
-              addCoord({lat: latitude, lng: longitude})
-              console.log(latitude,longitude)       
+
+              addCoord(coords)
+              console.log(latitude,longitude)    
+           
     // not working needs to show the map upon click (See class=Gmap in HTML)
-    $("#buttonSearch").on("click", () =>{
-        $(".Gmap").load();
-    })
-    // Not Working
     // $("#buttonSearch").on("click", () =>{
-    //     $(".Gmap").fadeToggle();
-    // }) 
+    //     $(".Gmap").load();
+    // })
+    // Not Working
+    $("#buttonSearch").on("click", () =>{
+        $(".Gmap").slideDown();
+    }) 
     $('#ExpandButton1').click(function(){
         $('#ExpandText1').toggle('slow');
-        $("#Expandbutton1").html("Show less");
     });  
     $('#ExpandButton2').click(function(){
         $('#ExpandText2').toggle('slow');
-        $("#Expandbutton2").html("Show less");
     });  
     $('#ExpandButton3').click(function(){
         $('#ExpandText3').toggle('slow');
-        $("#Expandbutton3").html("Show less");
-        }
     });  
 })
 //how to click on and when click again than object will hide?
