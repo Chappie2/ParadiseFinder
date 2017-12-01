914e6ddcefac567a0d583a705cd689359b95dc5f
Chappie2 <32395880+Chappie2@users.noreply.github.com>
2017-12-01 01:45:14 +0100

    clean up

--- Awesome.js
+++ Awesome.js
@@ -1,20 +1,26 @@
-var latitude = 51.9166667;
-var longitude = 4.5;
+var latitude = 23.1135925;
+var longitude = -82.36659559999998;
 $(() => {
     var ac = new google.maps.places.Autocomplete(document.getElementById('Banana'));
-
     console.log(ac);
     google.maps.event.addListener(ac, 'place_changed', function(){
         var place = ac.getPlace();
-        var latitude = place.geometry.location.lat();
-        var longitude = place.geometry.location.lng();
+         latitude = place.geometry.location.lat();
+         longitude = place.geometry.location.lng();
         console.log(place.formatted_address);
         console.log(place.url);
         console.log(place.geometry.location);
         console.log(latitude);
         console.log(longitude);
     });
-    
+    // not working needs to show the map upon click (See class=Gmap in HTML)
+    $("#buttonSearch").on("click", () =>{
+        $(".Gmap").load();
+    })
+    // Not Working
+    // $("#buttonSearch").on("click", () =>{
+    //     $(".Gmap").fadeToggle();
+    // }) 
     $('#ExpandButton1').click(function(){
         $('#ExpandText1').toggle('slow');
     });  
